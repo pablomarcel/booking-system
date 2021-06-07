79b1bcb7cbb3bc40f880cda50c27d88311cdc324
pablomarcel <pablomarcel@outlook.com>
2021-06-06 21:19:46 -0700

    initial commit

---
+++ src/booking_system/booking_system.py
@@ -0,0 +1,11 @@
+import sys
+import pyfiglet
+import pandas as pd
+import numpy as np
+from tabulate import tabulate
+import dateutil
+import datetime
+import re
+
+result = pyfiglet.figlet_format("b o o k i n g  s y s t e m", font="slant")
+strStatus = ""
---
+++ tests/test_booking_system.py
@@ -0,0 +1,9 @@
+import booking_system.booking_system
+import pandas as pd
+from pandas._testing import assert_frame_equal
+import re
+
+# next employee id testing
+
+def test_1():
+    pass
