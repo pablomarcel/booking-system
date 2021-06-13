3c1e64f897f1baa74b6d8c5f35723e27c0471924
pablomarcel <pablomarcel@outlook.com>
2021-06-12 21:52:10 -0700

    completed all 15 tests

--- tests/test_booking_system.py
+++ tests/test_booking_system.py
@@ -43,6 +43,8 @@ def test_validate_booking_5():
     assert booking_system.ProcessingClasses.FileProcessor.validate_booking(book1) == True
     pass
 
+# validate if the system generates correct shipping options
+
 def test_generate_booking_quote_1():
     objP1 = booking_system.DataClasses.Customer('Pablo', 'Marcel')
     objPac1 = booking_system.DataClasses.Package('Dangerous', 5.0, 120.0, '07/01/21')
@@ -70,3 +72,86 @@ def test_generate_booking_quote_4():
     book1 = booking_system.DataClasses.Booking(objP1, objPac1)
     assert booking_system.ProcessingClasses.FileProcessor.generate_booking_quote(book1) == (True, True, False, False, True)
     pass
+
+def test_generate_booking_id():
+    df = pd.read_csv("src\\booking_system\\bookings.csv")
+
+    assert booking_system.ProcessingClasses.FileProcessor.generate_booking_id(df) == 10000003
+
+def test_customer():
+
+    first = 'Beatrix'
+    last = 'T'
+    customer1 = booking_system.DataClasses.Customer(first,last)
+
+    assert customer1.first_name == first
+    assert customer1.last_name == last
+
+def test_package():
+
+    contents = 'Dangerous'
+    weight = 5.0
+    volume = 120.0
+    deliveryDate = '07/01/21'
+    package1 = booking_system.DataClasses.Package(contents, weight, volume, deliveryDate)
+
+    assert package1.content == contents
+    assert package1.weight == weight
+    assert package1.volume == volume
+    assert package1.delivery_date == deliveryDate
+
+def test_booking():
+
+    first = 'Beatrix'
+    last = 'T'
+    contents = 'Dangerous'
+    weight = 5.0
+    volume = 120.0
+    deliveryDate = '07/01/21'
+    customer1 = booking_system.DataClasses.Customer(first, last)
+    package1 = booking_system.DataClasses.Package(contents, weight, volume, deliveryDate)
+    booking1 = booking_system.DataClasses.Booking(customer1, package1)
+
+    assert booking1.content == contents
+    assert booking1.weight == weight
+    assert booking1.volume == volume
+    assert booking1.delivery_date == deliveryDate
+
+def test_shipping():
+
+    method = 'air'
+    cost = '55'
+    shipping1 = booking_system.DataClasses.Shipping(method, cost)
+
+    assert shipping1.shipping_method == method
+    assert shipping1.cost == cost
+
+def test_booking_quote():
+
+    first = 'Beatrix'
+    last = 'T'
+    contents = 'Dangerous'
+    weight = 8.0
+    volume = 10.0
+    method = 'truck'
+    cost = '55'
+    deliveryDate = '07/01/21'
+    customer1 = booking_system.DataClasses.Customer(first, last)
+    package1 = booking_system.DataClasses.Package(contents, weight, volume, deliveryDate)
+    booking1 = booking_system.DataClasses.Booking(customer1, package1)
+    shipping1 = booking_system.DataClasses.Shipping(method, cost)
+
+    booking_quote1 = booking_system.DataClasses.BookingQuote(customer1,package1,shipping1)
+
+    assert booking_quote1.first_name == first
+    assert booking_quote1.last_name == last
+    assert booking_quote1.content == contents
+    assert booking_quote1.shipping_method == method
+    assert booking_quote1.cost == cost
+    assert booking_quote1.delivery_date == deliveryDate
+    assert booking_quote1.weight == weight
+    assert booking_quote1.volume == volume
+
+
+
+
